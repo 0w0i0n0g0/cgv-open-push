8ab5ccf8114c33f305636120fe9d5191e3241023
0w0i0n0g0 <0w0i0n0g0@gmail.com>
2024-03-31 11:24:34 +0900

    Rename functions

--- src/cgv_open_push_function.py
+++ src/cgv_open_push_function.py
@@ -7,7 +7,7 @@ import logging
 from cgv_open_push_global_variable import ntfy_token
 
 # request의 응답 객체의 시간과 현재시간의 차이를 계산
-def get_time_difference(response):
+def calculate_response_delay(response):
     # 응답 헤더에서 날짜를 추출
     response_time_str = response.headers['Date']
     # 추출한 날짜 문자열을 datetime 객체로 변환
@@ -19,18 +19,18 @@ def get_time_difference(response):
     # 시간 차이를 반환
     return time_difference
 
-# cgv에서 영화 예매 정보 가져오기
-def send_curl_to_cgv_multiple(url, cookies, headers, json_data, target_name):
+# cgv에서 예매 정보 가져오기
+def get_request_to_cgv_api(url, cookies, headers, json_data, target_name):
     response = requests.post(
         url = url,
-        cookies=cookies,
-        headers=headers,
-        json=json_data,
-        verify=False,
+        cookies = cookies,
+        headers = headers,
+        json = json_data,
+        verify = False,
     )
     # 응답 본문 추출
     response_body = response.content
-    logging.info(f'{target_name} 서버가 응답에 걸린 시간 : {get_time_difference(response)}')
+    logging.info(f'{target_name} response delay : {calculate_response_delay(response)}')
     # UTF-8 디코딩
     response_text = response_body.decode('utf-8-sig')
     # JSON 데이터 파싱
@@ -40,8 +40,8 @@ def send_curl_to_cgv_multiple(url, cookies, headers, json_data, target_name):
     # 응답결과 리턴
     return data_value
 
-# 변경사항 푸시알림 보내기
-def send_ntfy_push(result, target_name):
+# 예매 오픈 푸시 알림 보내기
+def send_open_push(result :str, target_name :str):
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
         # ntfy 서버 토큰
@@ -51,25 +51,25 @@ def send_ntfy_push(result, target_name):
     response = requests.post(f'http://serverkorea.duckdns.org/{target_name}', headers=headers, data=data)
     logging.info(f'{target_name} 서버의 send_ntfy_push : {response}')
 
-# 정상작동 확인용 푸시알림 보내기
-def send_ntfy_push_health_check(text, target_name):
+# 개인 ntfy에 푸시 알림 보내기
+def send_push_to_private_ntfy(text :str, target_name :str):
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
         # ntfy 서버 토큰
         'Authorization': ntfy_token,
     }
-    data = f'{target_name}\n{text}'.encode()
+    data = text.encode()
     response = requests.post(f'http://serverkorea.duckdns.org/SERVER', headers=headers, data=data)
-    logging.info(f'{target_name} 서버의 send_ntfy_push_health_check : {response}')
+    logging.info(f'{target_name} send_push_to_private_ntfy : {text.strip()} : {response}')
 
-# 서버 정상작동 푸시알림 보내기
-def send_ntfy_push_server(string, target_name):
+# ntfy에 푸시 알림 보내기
+def send_push_to_ntfy(text :str, target_name :str):
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
     }
-    data = f'{string}'.encode()
+    data = text.encode()
     response = requests.post('http://ntfy.sh/CGVOPENPUSHSERVER', headers=headers, data=data)
-    logging.info(f'{target_name} 서버의 send_ntfy_push_server : {response}')
+    logging.info(f'{target_name} send_push_to_ntfy : {text.strip()} : {response}')
 
 # XML 객체를 받아서 PlayDays 태그를 XML 객체로 반환
 def extract_playdays(xml_string):
@@ -78,13 +78,6 @@ def extract_playdays(xml_string):
     # 'PlayDays' 태그를 찾아서 해당 내용을 리턴
     playdays = root.find('.//PlayDays')
     return ET.tostring(playdays, encoding='unicode', method='xml') if playdays is not None else None
-
-# 문자열을 받아서 FORMAT_DATE 태그 사이의 문자열을 모두 반환
-def extract_format_date(xml_string):
-    # 정규 표현식을 사용하여 <FORMAT_DATE>과 </FORMAT_DATE> 사이의 모든 문자열 찾기
-    pattern = re.compile(r'<FORMAT_DATE>(.*?)</FORMAT_DATE>')
-    # 찾은 문자열을 ', '로 구분하여 하나의 문자열로 연결하여 반환
-    return ', '.join(pattern.findall(xml_string))
 
 # 문자열과 태그를 받아서 해당 태그 사이의 문자열을 모두 반환
 def extract_xml_content_by_tag(xml_string, tag):
