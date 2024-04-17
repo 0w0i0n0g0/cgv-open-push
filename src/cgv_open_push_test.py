6f5a160162e751a10e0693e498291ff3028ddedf
0w0i0n0g0 <0w0i0n0g0@gmail.com>
2024-04-17 09:53:52 +0900

    Update cgv_open_push_test.py

--- src/cgv_open_push_test.py
+++ src/cgv_open_push_test.py
@@ -4,17 +4,28 @@ from cgv_open_push_global_variable import *
 
 class CgvOpenPushTest(unittest.TestCase):
 
+    # 테스트 시작
+    def setUp(self):
+        print('\n')
+
     # send_curl_to_cgv_multiple 테스트
     def test_send_curl_to_cgv_multiple(self):
         for data in enumerate(json_data):
-            response = send_curl_to_cgv_multiple(url, cookies, headers, data[1], target_name[data[0]])
-            print(extract_xml_content_by_tag(response, "THEATER_NM"))
-            print(extract_xml_content_by_tag(response, "RATING_NM"))
-            print(extract_xml_content_by_tag(response, "MOVIE_GROUP_NM"))
+            print(f"test_send_curl_to_cgv_multiple to {target_name[data[0]]}\n")
+            response = get_request_to_cgv_api(url, cookies, headers, data[1], target_name[data[0]])
+            print(f"위치 : {extract_text_between_tag(response, "THEATER_NM")}")
+            print(f"유형 : {extract_text_between_tag(response, "RATING_NM")}")
+            print(f"영화 : {extract_text_between_tag(response, "MOVIE_GROUP_NM")}")
 
-    # send_ntfy_push_health_check 테스트
-    def test_send_ntfy_push_health_check(self):
-        send_ntfy_push_health_check("test", "test")
+    # send_push_to_private_ntfy 테스트
+    def test_send_push_to_private_ntfy(self):
+        res = send_push_to_private_ntfy(f"test_send_push_to_private_ntfy\n푸시 알림 테스트입니다.", "test_send_push_to_private_ntfy")
+        print(f"test_send_push_to_private_ntfy : {res}")
+
+    # send_push_to_ntfy 테스트
+    def test_send_push_to_ntfy(self):
+        res = send_push_to_ntfy(f"test_send_push_to_ntfy\n푸시 알림 테스트입니다.", "test_send_push_to_ntfy")
+        print(f"test_send_push_to_ntfy : {res}")
 
 if __name__ == '__main__':
     unittest.main()
