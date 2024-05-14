1bc56d1d9592f1f5d3b46caa1986b8fb12a67409
0w0i0n0g0 <0w0i0n0g0@gmail.com>
2024-05-14 21:04:36 +0900

    Update cgv_open_push_announcement.py

--- src/cgv_open_push_announcement.py
+++ src/cgv_open_push_announcement.py
@@ -1,12 +1,24 @@
 # 전체 topic에 공지사항 발송
 
 from cgv_open_push_function import *
-from cgv_open_push_global_variable import *
 
 # 공지사항 내용
 text = ""
 
-send_open_push_announcement(text, "YONGSAN-IMAX")
+# 종료된 구독
+previous_topics = [
+    "DUNE-PART2-YONGSAN-IMAX",
+    "GODZILLA-KONG",
+    ]
+for topic in previous_topics:
+    send_open_push_announcement(text, topic)
 
-for topic in enumerate(target_name):
+# 현재 구독
+current_topics = [
+    "YONGSAN-IMAX",
+    "HAIKU-YONGSAN-IMAX",
+    "APES-YONGSAN-IMAX",
+    "FURIOSA-YONGSAN-IMAX",
+    ]
+for topic in current_topics:
     send_open_push_announcement(text, topic)
