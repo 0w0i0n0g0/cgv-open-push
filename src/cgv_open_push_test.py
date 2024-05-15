e11a2380b12350968809d880df85204703522202
0w0i0n0g0 <0w0i0n0g0@gmail.com>
2024-05-15 19:03:21 +0900

    Update cgv_open_push_test.py

--- src/cgv_open_push_test.py
+++ src/cgv_open_push_test.py
@@ -13,9 +13,12 @@ class CgvOpenPushTest(unittest.TestCase):
         for data in enumerate(json_data):
             print(f"test_send_curl_to_cgv_multiple to {target_name[data[0]]}\n")
             response = get_request_to_cgv_api(url, cookies, headers, data[1], target_name[data[0]])
-            print(f"위치 : {extract_text_between_tag(response, "THEATER_NM")}")
-            print(f"유형 : {extract_text_between_tag(response, "RATING_NM")}")
-            print(f"영화 : {extract_text_between_tag(response, "MOVIE_GROUP_NM")}")
+            위치 = extract_text_between_tag(response, "THEATER_NM")
+            print(f"위치 : {위치}")
+            유형 = extract_text_between_tag(response, "RATING_NM")
+            print(f"유형 : {유형}")
+            영화 = extract_text_between_tag(response, "MOVIE_GROUP_NM")
+            print(f"영화 : {영화}")
 
     # send_push_to_private_ntfy 테스트
     def test_send_push_to_private_ntfy(self):
