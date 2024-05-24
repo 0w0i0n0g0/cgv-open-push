1340f667bd575f04719a1ba37bb56223e7e25201
0w0i0n0g0 <0w0i0n0g0@gmail.com>
2024-05-25 00:42:01 +0900

    Update cgv_open_push_test.py

--- src/cgv_open_push_test.py
+++ src/cgv_open_push_test.py
@@ -10,15 +10,25 @@ class CgvOpenPushTest(unittest.TestCase):
 
     # send_curl_to_cgv_multiple 테스트
     def test_send_curl_to_cgv_multiple(self):
-        for data in enumerate(json_data):
-            print(f"test_send_curl_to_cgv_multiple to {target_name[data[0]]}\n")
-            response = get_request_to_cgv_api(url, cookies, headers, data[1], target_name[data[0]])
+        for data in enumerate(movie_json_data):
+            print(f"test_send_curl_to_cgv_multiple to {movie_target_name[data[0]]}\n")
+            response = get_request_to_cgv_api(movie_url, movie_cookies, movie_headers, data[1], movie_target_name[data[0]])
             위치 = extract_text_between_tag(response, "THEATER_NM")
             print(f"위치 : {위치}")
             유형 = extract_text_between_tag(response, "RATING_NM")
             print(f"유형 : {유형}")
             영화 = extract_text_between_tag(response, "MOVIE_GROUP_NM")
             print(f"영화 : {영화}")
+            print('\n')
+
+        for data in enumerate(screen_json_data):
+            print(f"test_send_curl_to_cgv_multiple to {screen_target_name[data[0]]}\n")
+            response = get_request_to_cgv_api(screen_url, screen_cookies, screen_headers, data[1], screen_target_name[data[0]])
+            위치 = extract_text_between_tag(response, "THEATER_NM")
+            print(f"위치 : {위치}")
+            유형 = extract_text_between_tag(response, "RATING_NM")
+            print(f"유형 : {유형}")
+            print('\n')
 
     # send_push_to_private_ntfy 테스트
     def test_send_push_to_private_ntfy(self):
