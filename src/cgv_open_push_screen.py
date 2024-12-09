e04773d124516df894ab9dbcc4320d83de2d8677
0w0i0n0g0 <0w0i0n0g0@gmail.com>
2024-12-09 14:03:58 +0900

    FEAT : 디스코드 알림으로 변경.

--- src/cgv_open_push_screen.py
+++ src/cgv_open_push_screen.py
@@ -1,16 +1,18 @@
 import os
 import sys
 import time
+import queue
 from cgv_open_push_function import *
 from diff_match_patch import diff_match_patch
 
 # 특별관 업데이트 내역 확인 로직
-def screen_main(url, cookies, headers, json_data, target_name):
+def screen_main(url, cookies, headers, json_data, target_name, message_queue):
     try:
         # 첫 응답 저장
         response1 = get_request_to_cgv_api(url, cookies, headers, json_data, target_name)
         response2 = ""
         while True:
+            time.sleep(5)
             # 2번에 새 응답 저장
             response2 = get_request_to_cgv_api(url, cookies, headers, json_data, target_name)
             위치 = extract_text_between_tag(response2, "THEATER_NM")
@@ -45,14 +47,12 @@ def screen_main(url, cookies, headers, json_data, target_name):
                     save_log_info(f'{target_name} added item : {deleted_result.encode()}')
                     # 추가된 변경사항 푸시알림 보내기
                     try:
-                        send_open_push(str(added_result), target_name)
+                        message_queue.put([target_name, "**예매 오픈 알림** : " + str(added_result)])
                     except Exception as e:
-                        send_push_to_ntfy(f"{target_name}\n서버에서 푸시 알림 전송 중 오류 발생🚨\n{e}", target_name)
-                        send_push_to_private_ntfy(f"{target_name}\n서버에서 푸시 알림 전송 중 오류 발생🚨\n{e}", target_name)
                         save_log_error(f'{target_name} error when sending open push : {e}')
                         # 5초 대기 후 다시 알림 보내기 시도
                         time.sleep(5)
-                        send_open_push(str(added_result), target_name)
+                        message_queue.put([target_name, "**예매 오픈 알림** : " + str(added_result)])
                 #삭제된 요소가 있으면
                 if deleted_result != "":
                     #로그만 남기기
@@ -60,11 +60,9 @@ def screen_main(url, cookies, headers, json_data, target_name):
                 # response1 값은 변경된 값으로 초기화
                 response1 = response2
             # 5분마다 새로고침
-            time.sleep(300)
+            time.sleep(295)
     # 오류 발생 시
     except Exception as e:
-        send_push_to_ntfy(f"{target_name}\n서버에서 오류 발생🚨\n{e}", target_name)
-        send_push_to_private_ntfy(f"{target_name}\n서버에서 오류 발생🚨\n{e}", target_name)
         save_log_error(f'{target_name} error : {e}')
         # 다시 실행
         os.execl(sys.executable, sys.executable, *sys.argv)
